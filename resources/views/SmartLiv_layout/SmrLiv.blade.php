b525bb61bb8a83386f6663e0318d67a350775c94
Maisho20 <bahajazz70@gmail.com>
2024-02-23 09:17:44 +0700

    Update Smart Liv

--- resources/views/SmartLiv_layout/SmrLiv.blade.php
+++ resources/views/SmartLiv_layout/SmrLiv.blade.php
@@ -21,8 +21,8 @@
     </script>
 
     <!-- Favicons -->
-    <link href="{{ asset('img/favicon.png') }}" rel="icon">
-    <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="{{ asset('img/Smart/Logo_Kabupaten_Malang_-_Seal_of_Malang_Regency.svg.png') }}" rel="icon">
+    <link href="{{ asset('img/Smart/Logo_Kabupaten_Malang_-_Seal_of_Malang_Regency.svg.png') }}" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link
@@ -53,22 +53,16 @@
         <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
             <a href="{{ route('home') }}" class="logo d-flex align-items-center">
-                <img src="{{ asset('img/logo.png') }}" alt="">
-                <span>Web & Carto</span>
+                <img src="{{ asset('img/Smart/Logo_Kabupaten_Malang_-_Seal_of_Malang_Regency.svg.png') }}"
+                    alt="">
+                <span>Smart Living</span>
             </a>
 
             <nav id="navbar" class="navbar">
                 <ul>
-                    <li><a class="nav-link scrollto" href="#about">About</a></li>
-                    <li><a class="nav-link scrollto" href="#features">Features</a></li>
-                    <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                    <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
-                    <li><a class="nav-link scrollto" href="#portfolio">Smart City</a></li>
-                    <li><a class="nav-link scrollto" href="#recent-blog-posts">Blog</a></li>
-                    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-                    <li><a class="getstarted scrollto"
-                            href="mailto:tounsils@gmail.com?subject=Contacting throu tn76.com&body=I need you to ...">Get
-                            Started</a></li>
+                    <li><a class="nav-link scrollto" href="#about">Tentang</a></li>
+                    <li><a class="nav-link scrollto" href="#features">Tujuan Program</a></li>
+                    <li><a class="nav-link scrollto" href="#services">Kendala V/S Penyelesaian</a></li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav>
@@ -76,6 +70,213 @@
 
         </div>
     </header>
+
+    <!-- End Header -->
+
+    <!-- start reusable part -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="about" class="hero d-flex align-items-center">
+
+        <div id="hero" class="container">
+            <div class="row">
+                <div class="col-lg-6 d-flex flex-column justify-content-center">
+                    <h1 data-aos="fade-up">E-AGRIPROP</h1>
+                    <p style="font-size: 20px!important; text-indent: 45px!important;" data-aos="fade-up"
+                        data-aos-delay="400">
+                        <b>Desaku Tuntas,</b>
+                        program administrasi kependudukan Dinas Dukcapil Kabupaten Malang, memudahkan masyarakat dengan
+                        layanan gratis melalui aplikasi website. Diluncurkan pada 9 Maret 2021, kini melibatkan 298
+                        Desa/Kelurahan. Aplikasi baru diperkenalkan pada 7 Juli 2022, menunjukkan komitmen perbaikan.
+                        Hingga September 2022, tercatat rata-rata 500 pengajuan per hari, dengan jaminan penyelesaian
+                        dokumen adminduk dalam 5 hari kerja. Program ini terus berupaya meningkatkan aksesibilitas dan
+                        efisiensi pelayanan administrasi kependudukan.
+                    </p>
+                    <div data-aos="fade-up" data-aos-delay="600">
+                        <div class="text-center text-lg-start">
+                            <a href="http://agriprop.malangkab.go.id/" target="_blank"
+                                class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                                <span>Get Started</span>
+                                <i class="bi bi-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+                    <img src="{{ asset('img/Smart/Liv/Liv1.png') }}" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- End Hero -->
+
+    <!-- ======= Features Section ======= -->
+    <section class="features">
+
+        <div id="features" class="container" data-aos="fade-up">
+
+            <!-- Feature Icons -->
+            <div class="row feature-icons" data-aos="fade-up">
+                <h3>Inovasi-inovasi Pelayanan</h3>
+
+                <div class="row">
+
+                    <div class="col-xl-4 text-center" data-aos="fade-right" data-aos-delay="100">
+                        <img src="{{ asset('img/features-3.png') }}" class="img-fluid p-4" alt="">
+                    </div>
+
+                    <div class="col-xl-8 d-flex content">
+                        <div class="row align-self-center gy-4">
+
+
+                            <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+                                <i class="ri-heart-pulse-fill"></i>
+                                <div>
+                                    <h4>Pertama</h4>
+                                    <p>
+                                        Screening Penyakit Oleh Kader Kesehatan.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+                                <i class="ri-survey-fill"></i>
+                                <div>
+                                    <h4>Kedua</h4>
+                                    <p>
+                                        Sistem Penanggulangan Gawat Darurat Terpadu, dan KLB
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 icon-box" data-aos="fade-up">
+                                <i class="ri-user-follow-line"></i>
+                                <div>
+                                    <h4>Ketiga</h4>
+                                    <p>
+                                        Layanan eBooking, Riwayat Kesehatan dan Kepesertaan BPJS.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
+                                <i class="ri-collage-line"></i>
+                                <div>
+                                    <h4>Keempat</h4>
+                                    <p>
+                                        Telekonsultasi, Rujukan Terintegrasi.
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- End Feature Icons -->
+
+        </div>
+
+    </section>
+    <!-- End Features Section -->
+
+    <!-- ======= Features Section ======= -->
+    <section id="services" class="features">
+
+        <div class="container" data-aos="fade-up">
+
+            <!-- Feature Tabs -->
+            <div class="row feture-tabs" data-aos="fade-up">
+                <div class="col-lg-7">
+                    <h3>
+                        Kendala <b>V/S</b> Penyelesaian Smart Governance
+                    </h3>
+
+                    <!-- Tabs -->
+                    <ul class="nav nav-pills mb-3">
+                        <li>
+                            <a class="nav-link active" data-bs-toggle="pill" href="#tab1">Kendala / Kebutuhan
+
+                            </a>
+                        </li>
+                        <li>
+                            <a class="nav-link" data-bs-toggle="pill" href="#tab2">Pelaksanaan & Penyelesaian
+
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- End Tabs -->
+
+                    <!-- Tab Content -->
+                    <div class="tab-content">
+
+                        <div class="tab-pane fade show active" id="tab1">
+                            <p style="text-indent: 45px!important;">
+                                Kendala yang dalam memberikan Standart Pelayanan Minimal bidang Kesehatan kepada
+                                2,65Juta Penduduk Kabupaten Malang, Jumlah Sumberdaya Manusia Tenaga Kesehatan masih
+                                sangat kurang, Integrasi Program serta Inovasi sangat diperlukan, terutama pemerintah
+                                Desa / Kelurahan upaya melatih Kader menjadi Kader Kesehatan telah dilakukan, tentunya
+                                masih sangat perlu dukungan dari Lintas OPD dalam menyelesaikan Target dan Capaian
+                                Program dan Visi Misi Daerah
+                            </p>
+                        </div>
+                        <!-- End Tab 1 Content -->
+
+                        <div class="tab-pane fade show" id="tab2">
+                            <p style="text-indent: 45px!important;">
+                                Integrasi Program dan Peran Aktif Litas Sektor dalam Penyelesaian masalah pada Smart
+                                Living serta Fokus Daerah :
+                            </p>
+                            <div class="d-flex align-items-center mb-2">
+                                <i class="bi bi-dot"></i>
+                                <h4>Integrasi Program Dengan Lintas Sektor Dalam Penanggulangan Stunting</h4>
+                            </div>
+                            <div class="d-flex align-items-center mb-2">
+                                <i class="bi bi-dot"></i>
+                                <h4>Peningkatan peran serta Dinas Sosial dalam pengajuan data DTKS ke dalam usulan PBIN
+                                    dan mendorong Kepesertaan mandiri untuk peserta non Miskin</h4>
+                            </div>
+                            <div class="d-flex align-items-center mb-2">
+                                <i class="bi bi-dot"></i>
+                                <h4>Peningkatan Peran Desa dan Kecamatan dalam pendataan di aplikasi SIK-NG DINSOS.</h4>
+                            </div>
+                            <div class="d-flex align-items-center mb-2">
+                                <i class="bi bi-dot"></i>
+                                <h4>Pelayanan Administrasi Kependudukan PBIN/ PBID belum punya NIK dan verifikasi data
+                                    pasien meninggal dunia / pindah</h4>
+                            </div>
+                        </div>
+                        <!-- End Tab 2 Content -->
+                    </div>
+
+                </div>
+
+                <div class="col-lg-5">
+                    <img src="{{ asset('img/Smart/Eco/Eco2.png') }}" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- End Features Section -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+
+        <div class="container">
+            <div class="copyright">
+                &copy;
+                <script>
+                    document.write(new Date().getFullYear())
+                </script> Copyright <strong><span>All Rights Reserved</span></strong>.
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>

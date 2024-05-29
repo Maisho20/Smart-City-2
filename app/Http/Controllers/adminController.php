fe3030a27472da9be7a07e082da17629db63fe20
Maisho20 <bahajazz70@gmail.com>
2024-05-29 22:10:15 +0700

    Update admin controller

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\home;
+use App\Models\DataBerita;
+use App\Models\DataDokumen;
 
 class adminController extends Controller
 {
     public function index()
     {
         $homeDatas = home::all();
+        $beritaDatas = DataBerita::all();
+        $dataDokumens = DataDokumen::all();
 
         // dd($homeDatas);
 
-        return view('Admin_view.admin', compact('homeDatas'));
+        return view('Admin_view.admin', compact('homeDatas', 'beritaDatas', 'dataDokumens'));
     }
 }

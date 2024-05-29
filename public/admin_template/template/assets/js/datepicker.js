d90a85b6eb361c176ed20ace64feaf1da2d7e0b2
Maisho20 <bahajazz70@gmail.com>
2024-05-29 16:58:33 +0700

    Add admin template

---
+++ public/admin_template/template/assets/js/datepicker.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd', // Format tanggal yang diinginkan (YYYY-MM-DD)
+        autoclose: true, // Menutup kalender setelah memilih tanggal
+        orientation: 'bottom' // Posisi tampilan kalender (bawah)
+    });
+});
+
+// JavaScript Datepicker

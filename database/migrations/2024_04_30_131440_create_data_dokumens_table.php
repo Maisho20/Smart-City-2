266d4ff9d6e8aec3575e8584e7aa86e2acd73dde
Maisho20 <bahajazz70@gmail.com>
2024-06-06 11:23:32 +0700

    Update docs table

--- database/migrations/2024_04_30_131440_create_data_dokumens_table.php
+++ database/migrations/2024_04_30_131440_create_data_dokumens_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('data_dokumens', function (Blueprint $table) {
             $table->id()->autoIncrement()->unique();
+            $table->string('image');
             $table->string('judul');
             $table->string('deskripsi');
             $table->string('file');

77ec8f3b2b4bebe3bcd2fd6a7d66990ced509782
Maisho20 <bahajazz70@gmail.com>
2024-06-06 11:23:43 +0700

    Update docs seeder

--- database/seeders/DataDokumenSeeder.php
+++ database/seeders/DataDokumenSeeder.php
@@ -14,27 +14,31 @@ class DataDokumenSeeder extends Seeder
     public function run(): void
     {
         DB::table('data_dokumens')->insert([
+            'image' => 'docs/tampilan dokumen.jpg',
             'judul' => 'Dokumen 1',
             'deskripsi' => 'Deskripsi Dokumen 1',
-            'file' => "Dokumen berisikan tentang informasi Smart City.pdf",
+            'file' => "docs/Dokumen berisikan tentang informasi Smart City.pdf",
         ]);
 
         DB::table('data_dokumens')->insert([
+            'image' => 'docs/tampilan dokumen.jpg',
             'judul' => 'Dokumen 2',
             'deskripsi' => 'Deskripsi Dokumen 2',
-            'file' => "Dokumen berisikan tentang informasi Smart City.pdf",
+            'file' => "docs/Dokumen berisikan tentang informasi Smart City.pdf",
         ]);
 
         DB::table('data_dokumens')->insert([
+            'image' => 'docs/tampilan dokumen.jpg',
             'judul' => 'Dokumen 3',
             'deskripsi' => 'Deskripsi Dokumen 3',
-            'file' => "LAPORAN_EVALUASI_SC_2022_Kab Malang.pdf",
+            'file' => "docs/LAPORAN_EVALUASI_SC_2022_Kab Malang.pdf",
         ]);
 
         DB::table('data_dokumens')->insert([
+            'image' => 'docs/tampilan dokumen.jpg',
             'judul' => 'Dokumen 4',
             'deskripsi' => 'Deskripsi Dokumen 4',
-            'file' => "LAPORAN_EVALUASI_SC_2022_Kab Malang.pdf",
+            'file' => "docs/LAPORAN_EVALUASI_SC_2022_Kab Malang.pdf",
         ]);
     }
 }

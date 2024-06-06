31f65d06d93e3e2264fe90c5925fc216354dfdc1
Maisho20 <bahajazz70@gmail.com>
2024-06-06 11:23:07 +0700

    Update docs controller

--- app/Http/Controllers/adminDokumenController.php
+++ app/Http/Controllers/adminDokumenController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\DataDokumen;
+use Illuminate\Support\Facades\Storage;
 
 class adminDokumenController extends Controller
 {
@@ -26,21 +27,79 @@ class adminDokumenController extends Controller
         // dd($request);
         $request->validate([
             'judul' => 'required',
+            'image' => 'required|mimes:png,jpg,jpeg|max:2048',
             'deskripsi' => 'required',
             'file' => 'required|mimes:pdf|max:8192',
         ]);
 
         $dokumen = new DataDokumen();
+        // image
+        $files = $request->file('image');
+        $fileNames = 'docs/' . time() . '.' . $files->getClientOriginalName();
+        $files->storeAs($fileNames);
 
+        // dokumen
         $file = $request->file('file');
-        $fileName = 'file/dokumen/' . time() . '.' . $file->getClientOriginalExtension();
-        $file->storeAs('/public/', $fileName);
+        $fileName = 'docs/' . time() . '.' . $file->getClientOriginalName();
+        $file->storeAs($fileName);
 
         $dokumen->judul = $request->judul;
+        $dokumen->image = $fileNames;
         $dokumen->deskripsi = $request->deskripsi;
         $dokumen->file = $fileName;
         $dokumen->save();
 
         return redirect()->route('admin_dokumen')->with('Berhasil tambah dokumen!');
+    }
+
+    public function edit($id)
+    {
+        $dokumen = DataDokumen::find($id);
+
+        return view('Admin_view.dokumen_edit', compact('dokumen'));
+    }
+
+    public function update(Request $request,int $id)
+    {
+        // @dd($request);
+        $request->validate([
+            'judul' => 'required',
+            'image' => 'nullable|mimes:png,jpg,jpeg|max:2048',
+            'deskripsi' => 'required',
+            'file' => 'nullable|mimes:pdf|max:8192',
+        ]);
+
+        $dokumen = DataDokumen::where('id', $id)->first();
+        // image
+        if ($request->hasFile('image')) {
+            Storage::delete($dokumen->image);
+            $files = $request->file('image');
+            $fileNames = 'docs/' . time() . '.' . $files->getClientOriginalName();
+            $files->storeAs($fileNames);
+            $dokumen->image = $fileNames;
+        }
+
+        // dokumen
+        if ($request->hasFile('file')) {
+            Storage::delete($dokumen->file);
+            $file = $request->file('file');
+            $fileName = 'docs/' . time() . '.' . $file->getClientOriginalName();
+            $file->storeAs($fileName);
+            $dokumen->file = $fileName;
+        }
+
+        $dokumen->judul = $request->judul;
+        $dokumen->deskripsi = $request->deskripsi;
+        $dokumen->save();
+
+        return redirect()->route('admin_dokumen')->with('Berhasil update dokumen!');
+    }
+
+    public function delete($id)
+    {
+        $dokumen = DataDokumen::find($id);
+        $dokumen->delete();
+
+        return redirect()->route('admin_dokumen')->with('Berhasil hapus dokumen!');
     }
 }

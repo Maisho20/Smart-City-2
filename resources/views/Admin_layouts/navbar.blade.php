40043aee19c91a302cb09831d1cf7f0499d433ab
Maisho20 <bahajazz70@gmail.com>
2024-06-17 15:58:57 +0700

    Update navbar

--- resources/views/Admin_layouts/navbar.blade.php
+++ resources/views/Admin_layouts/navbar.blade.php
@@ -6,34 +6,13 @@
                     <div class="navbar-profile">
                         <img class="img-xs rounded-circle"
                             src="{{ asset('admin_template/template/assets/images/faces/face15.jpg') }}" alt="">
-                        <p class="mb-0 d-none d-sm-block navbar-profile-name">Admin</p>
+                        <p class="mb-0 d-none d-sm-block navbar-profile-name">{{ Auth::user()->name }}</p>
                         <i class="mdi mdi-menu-down d-none d-sm-block"></i>
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list"
                     aria-labelledby="profileDropdown">
                     <h6 class="p-3 mb-0">Actions</h6>
-                    <div class="dropdown-divider"></div>
-                    {{-- <a class="dropdown-item preview-item" href="{{ route('home') }}">
-                        <div class="preview-thumbnail">
-                            <div class="preview-icon bg-dark rounded-circle">
-                                <i class="mdi mdi-home text-info"></i>
-                            </div>
-                        </div>
-                        <div class="preview-item-content">
-                            <p class="preview-subject mb-1">Home</p>
-                        </div>
-                    </a> --}}
-                    {{-- <a class="dropdown-item preview-item">
-                        <div class="preview-thumbnail">
-                            <div class="preview-icon bg-dark rounded-circle">
-                                <i class="mdi mdi-settings text-success"></i>
-                            </div>
-                        </div>
-                        <div class="preview-item-content">
-                            <p class="preview-subject mb-1">Settings</p>
-                        </div>
-                    </a> --}}
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item preview-item"
                         onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
@@ -46,7 +25,7 @@
                             <p class="preview-subject mb-1">Log out</p>
                         </div>
                     </a>
-                    <form id="logout-form" action="{{ route('home') }}" method="GET">
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
                         @csrf</form>
                     <div class="dropdown-divider"></div>
                 </div>
